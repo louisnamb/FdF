b5ce0365f8feaf62c6caecb2dd36bf861accc114
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-07-05 12:01:19 +0930

    Add files via upload

--- includ/fdf.h
+++ includ/fdf.h
@@ -6,7 +6,7 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:46:29 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/06/29 15:16:45 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/07/05 11:29:09 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,23 @@ typedef struct	window_info {
 typedef struct	map_info {
 	int		rowcount;
 	int		columncount;
-	int		**elems;
-}	map
+	int		**arr;
+}	map;
 
 void	mmlx_put_pix(t_data *data, int x, int y, int colour);
 
-int	insidewin(int keycode, t_data *hook);
+int		insidewin(int keycode, t_data *hook);
 
 void	draw_square(t_data *mlx, int x, int x_bound, int height, char which);
 
-int	xbutton(int button, int x, int y, t_data *hook);
+int		xbutton(int button, int x, int y, t_data *hook);
 
-void read_map(char *filename);
+map		read_map(char *filename);
 
-char		**ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 
 char	*get_next_line(int fd);
 
 char	*ft_strjoin(char const *s1, char const *s2);
+
+int		ft_atoi(const char *str);
