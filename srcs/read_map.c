b7775b9ae4f6ca719855dcaa56cabd92f312b9c6
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-07-06 13:46:27 +0930

    Add files via upload

--- srcs/read_map.c
+++ srcs/read_map.c
@@ -6,89 +6,84 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 14:24:29 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/07/06 11:08:28 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/07/06 13:46:14 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-details read_map(char *filename)
+static int		ft_columnlen(char **arr)
 {
-	char	*result;
-	char	**split_str;
-	int		fd;
-	char	*path;
-	int		i;
-	i = 0;
-	char	*tmp;
-	char	*eachline;
-	int	column = 0;
-	int row = 0;
-	int	j = 0;
-	int p = 0;
+	int	i;
 
-	path = ft_strjoin("/Users/lnambaji/Documents/Cursus/fdf/", filename);
-	details *map = malloc(sizeof(details));
-	map->rowcount = 0;
-	map->columncount = 0;
-	map->arr = NULL;
+	i = 0;
+	while (arr[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+static int		ft_rowlen(char *path)
+{
+	int		rows;
+	char	*result;
+	int		fd;
+
 	fd = open(path, O_RDWR);
 	if (fd == -1) {
 		perror("Couldn't open the file. Try again.");
-		return (*map);
+		return (0);
 	}
 	result = get_next_line(fd);
-    while (result)
+	rows = 0;
+	while (result)
 	{
-		map->rowcount++;
-		tmp = result;
 	    result = get_next_line(fd);
+		rows++;
 	}
-	split_str = ft_split(tmp, ' ');
-	while (split_str[map->columncount])
-		map->columncount++;
 	close(fd);
+	return (rows);
+}
+
+details read_map(char *filename)
+{
+	char	**split_str;
+	int		fd;
+	char	*path;
+	details *map;
+	char	*eachline;
+
+	path = ft_strjoin("/Users/lnambaji/Documents/Cursus/fdf/", filename);
+	map = malloc(sizeof(details));
 	fd = open(path, O_RDWR);
+	if (fd == -1) {
+		perror("Couldn't open the file. Try again.");
+		return ((details){0, 0, 0, 0, NULL});//return (*map);
+	}
+	map->rowcount = ft_rowlen(path);
 	eachline = get_next_line(fd);
-	map->arr = malloc(sizeof(int *) * map->rowcount * map->columncount);
-	if (!map->arr)
-		return (*map);
-	while (eachline && row <= map->rowcount && column <= map->columncount)
+	map->columncount = ft_columnlen(ft_split(eachline, ' '));
+	map->arr = malloc(sizeof(int *) * map->rowcount);
+	if (!map->arr || !map->rowcount || !map->columncount)
+		return ((details){0, 0, 0, 0, NULL});//return (*map);
+	while (map->r_pos < map->rowcount)
 	{
-		printf("inside loop row: %d columncount: %d\n", row, map->columncount);
-		map->arr[row] = malloc(sizeof(int) * map->columncount);
+		map->arr[map->r_pos] = malloc(sizeof(int) * map->columncount);
 		split_str = ft_split(eachline, ' ');
-		if (!map->arr[row] || !split_str)
-			return (*map);
-		column = 0;
-		printf("louis: %d ft_isdigit: %d\n", ft_isdigit(split_str[column][0]), split_str[column][0]);
-		while (ft_isdigit(split_str[column][0]))
+		if (!map->arr[map->r_pos] || !split_str)
+			return ((details){0, 0, 0, 0, NULL});//return (*map);
+		map->c_pos = 0;
+		while (map->c_pos < map->columncount)
 		{
-			printf("gfsdfdsdfgdfg: %d ", map->arr[row][column]);
-			map->arr[row][column] = ft_atoi(split_str[column]);
-			column++;
+			map->arr[map->r_pos][map->c_pos] = ft_atoi(split_str[map->c_pos]);
+			map->c_pos++;
 		}
-		printf("df\n");
 		free(split_str);
 		split_str = NULL;
 		eachline = get_next_line(fd);
-		row++;
-	}	
-	//if (i == 1)
-	//	free(result);
-	//i++;
-	close(fd);
-	while (j < map->rowcount)
-	{
-		printf("j: %d map->rowcount: %d p: %d columncount: %d\n", j, map->rowcount, p, map->columncount);
-		while (p < map->columncount)
-		{
-			printf("out\n");
-			printf("%d", map->arr[j][p]);
-			p++;
-		}
-		printf("\n");
-		j++;
+		map->r_pos++;
 	}
+	close(fd);
 	return (*map);
 }
