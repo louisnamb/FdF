447a7f70dd72c0f4b90e7d16810735b82b43850a
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-07-06 13:49:45 +0930

    Add files via upload

--- srcs/event_handlers.c
+++ srcs/event_handlers.c
@@ -6,23 +6,19 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:46:04 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/07/06 12:41:09 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/07/06 13:22:53 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	insidewin(int keycode, t_data *hook)
+int	esc(int keycode, t_data *hook)
 {
 	if (keycode == 53)
 	{
-		printf("nwo\n");
 		mlx_destroy_window(hook->mlx, hook->win);
 		exit(0);
 	}
-	else 
-		printf("here\n");
-	printf("keycode: %d\n", keycode);
 	(void)keycode;
 	return (0);
 }
@@ -36,10 +32,20 @@ int key_hook(int keycode, t_data *hook)
 
 int	xbutton(int button, int x, int y, t_data *hook)
 {
-	if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50))// <= 1920) && (y >= 0 && y <= 1080))
+	if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50))
 		printf("button clicked\n");
 	(void)hook;
 	(void)button;
+	return (0);
+}
+
+int mouse_move(int x, int y, t_data *hook)
+{
+	if ((x >= 7 && x <= 20) && (y >= -19 && y <= -5))//find which X11 mask it is
+	{
+		mlx_destroy_window(hook->mlx, hook->win);
+		exit(0);
+	}
 	return (0);
 }
 
