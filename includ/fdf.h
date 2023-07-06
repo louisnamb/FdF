415f725756b9f2770678872ff9300f394dba7524
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-07-06 14:31:54 +0930

    Add files via upload

--- includ/fdf.h
+++ includ/fdf.h
@@ -6,7 +6,7 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:46:29 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/07/05 11:29:09 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/07/06 14:29:47 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,43 @@
 typedef struct	window_info {
 	void	*mlx;
 	void	*img;
+	void	*win;
 	char	*addr;
 	int		bitsperpix;
 	int		linelen;
 	int		endian;
-	void	*win;
 }	t_data;
 
 typedef struct	map_info {
 	int		rowcount;
 	int		columncount;
+	int		r_pos;
+	int		c_pos;
 	int		**arr;
-}	map;
+}	details;
 
-void	mmlx_put_pix(t_data *data, int x, int y, int colour);
+void		mmlx_put_pix(t_data *data, int x, int y, int colour);
 
-int		insidewin(int keycode, t_data *hook);
+int			esc(int keycode, t_data *hook);
 
-void	draw_square(t_data *mlx, int x, int x_bound, int height, char which);
+void		draw_square(t_data *mlx, int x, int x_bound, int height, char which);
 
-int		xbutton(int button, int x, int y, t_data *hook);
+int			mouse_move(int button, int x, int y, t_data *hook);
 
-map		read_map(char *filename);
+details		read_map(char *filename);
 
-char	**ft_split(char const *s, char c);
+char		**ft_split(char const *s, char c);
 
-char	*get_next_line(int fd);
+char		*get_next_line(int fd);
 
-char	*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strjoin(char const *s1, char const *s2);
 
-int		ft_atoi(const char *str);
+int			ft_atoi(const char *str);
+
+int			key_hook(int keycode, t_data *hook);
+
+int			ft_isdigit(int c);
+
+int			xbutton(t_data *hook);// int x, int y, t_data *hook);
+
+int			mouse_hook(int keycode, int button, int x, int y, t_data *hook);
