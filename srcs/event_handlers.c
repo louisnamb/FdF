e4d93e64a1ecccf9ea263f1eaf5eb8d209899f49
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-07-06 12:42:21 +0930

    Add files via upload

--- srcs/event_handlers.c
+++ srcs/event_handlers.c
@@ -6,7 +6,7 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:46:04 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/06/28 11:48:43 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/07/06 12:41:09 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 int	insidewin(int keycode, t_data *hook)
 {
-	mlx_destroy_window(hook->mlx, hook->win);
+	if (keycode == 53)
+	{
+		printf("nwo\n");
+		mlx_destroy_window(hook->mlx, hook->win);
+		exit(0);
+	}
+	else 
+		printf("here\n");
 	printf("keycode: %d\n", keycode);
 	(void)keycode;
-	exit(0);
+	return (0);
+}
+
+int key_hook(int keycode, t_data *hook)
+{
+	(void)hook;
+	printf("keycode: %d", keycode);
 	return (0);
 }
 
@@ -27,5 +40,14 @@ int	xbutton(int button, int x, int y, t_data *hook)
 		printf("button clicked\n");
 	(void)hook;
 	(void)button;
+	return (0);
+}
+
+int	mouse_hook(int keycode, int button, int x, int y, t_data *hook)
+{
+	printf("keycode: %d button: %d x: %d y: %d", keycode, button, x, y);
+//	printf("param:%d\n", (int)param);
+	mlx_destroy_window(hook->mlx, hook->win);
+	exit(0);
 	return (0);
 }
