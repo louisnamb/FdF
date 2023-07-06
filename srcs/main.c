c59af94fd209a178b4e63987e88e90145bb7bfb4
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-07-06 14:37:36 +0930

    Add files via upload

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 13:22:47 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/07/04 12:34:40 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/07/06 14:28:21 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,45 +17,26 @@ void	mmlx_put_pix(t_data *data, int x, int y, int colour)
 	char	*dst;
 
 	dst = data->addr + (y * data->linelen + x * (data->bitsperpix / 8));
-	/*
-pixels are just an array of pixel addresses. to index pixels on specific lines you use the above notation.
-y is the line (row) of the pixel. y * data->linelen is the linelen-th pixel
-x is the left most pixel. one pixel requires 4 bytes in memory
-	*/
-	//iterates over every line
-	//iterate over each pixel 0 + 4 * (desiredpix)
 	*(unsigned int *)dst = colour;
 }
 
-int main(int argc, char **argv)
+int main()//int argc, char **argv)
 {
-//	t_data	mlx;
-	int		i;
-	int		j;
-
-	i = 935;
-	j = 5;
-	if (argc == 2)
-		read_map(argv[1]);
-	else
-		write(1, "Error: Invalid arguments", 27);
-	return (1);
-	// mlx.mlx = mlx_init();
-	// mlx.win = mlx_new_window(mlx.mlx, 1920, 1080, "fdf");
-	// mlx.img = mlx_new_image(mlx.mlx, 1920, 1080);
-	// mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bitsperpix, &mlx.linelen, &mlx.endian);
-	// draw_square(&mlx, 935, 1035, 100, 'x');
-	// draw_square(&mlx, 935, 1035, 200, 'x');
-	// draw_square(&mlx, 935, 100, 200, 'y');
-	// draw_square(&mlx, 1035, 100, 200, 'y');
-	// mmlx_put_pix(&mlx, 80, 8, 0x000000FF);
-	// mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
-	// mlx_hook(mlx.win, 2, 1L<<0, insidewin, &mlx);
-	// mlx_hook(mlx.win, 4, 1L<<2, xbutton, &mlx);
-	// mlx_loop(mlx.mlx);
-//	return (0);
+	t_data	mlx;
+//	if (argc == 2)
+//		read_map(argv[1]);
+//	else
+//		write(1, "Error: Invalid arguments", 27);
+	//return (1);
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, 1920, 1080, "fdf");
+	mlx.img = mlx_new_image(mlx.mlx, 1920, 1080);
+	mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bitsperpix, &mlx.linelen, &mlx.endian);
+	mmlx_put_pix(&mlx, 80, 8, 0x000000FF);
+	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
+	mlx_mouse_hook(mlx.win, mouse_hook, mlx.mlx);
+	mlx_hook(mlx.win, 17, 1L<<5, xbutton, &mlx.img);
+	mlx_hook(mlx.win, 2, 1L<<0, esc, &mlx.img);
+	mlx_loop(mlx.mlx);
+	return (0);
 }
-/*
--	Draw a line
--	Get ESC button to quit program
-*/
