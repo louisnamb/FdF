8d6b76c55dc10dd9e64a98d35e40b48fbb6f1c69
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-08-04 14:00:09 +0930

    Add files via upload

--- draw_.c
+++ draw_.c
@@ -6,31 +6,61 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:55:20 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/06/29 12:41:24 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/08/04 13:31:52 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_square(t_data *mlx, int x, int x_bound, int height, char which)
+void	draw_square(t_data *mlx, int x, int x_bound, int height)//, char which)
 {
 	int	thickness;
-
+//	int x_original = x;
+//	int y_original = x_bound;
 	thickness = 5;
-	if (which == 'x')
+
+	while (thickness--)
 	{
-		while (thickness--)
-		{
-			while (x++ < x_bound)
-				mmlx_put_pix(mlx, x, height, 0x00FF0000);
-		}
+		while (x++ < x_bound)
+			mlx_pixel_put(mlx->mlx, mlx->win, x, x_bound, 0x00FF0000);
 	}
-	else
+			//mmlx_put_pix(mlx, x, height, 0x00FF0000);
+	while (thickness--)
 	{
-		while (thickness--)
-		{
-			while (x_bound++ < height)
-				mmlx_put_pix(mlx, x, x_bound, 0x00FF0000);
-		}
+		while (x_bound++ < height)
+			mlx_pixel_put(mlx->mlx, mlx->win, x, x_bound, 0x00FF0000);
 	}
+	//		mmlx_put_pix(mlx, x, x_bound, 0x00FF0000);
+}
+/*
+Use mlx_pixel_put to avoid screen tears and seg faults. goal is to create a square
+that you can use your wasd keys to move
+*/
+void	draw_point(t_data *mlx, int x, int y)
+{
+	mmlx_put_pix(mlx, x, y, 0x00FF0000);
+}
+//draw_line(mlx.mlx, mlx.win, 10,)
+int		draw_line(void *mlx, void *win, int startX, startY, int endX, int endY, int color)
+
+int		encode_colors(double red, double green, double blue)
+{
+	int	color;
+
+	color = 0;
+	color |= (int)(red >> 16) * 255;
+	color |= (int)(green >> 8) * 255;
+	color |= (int)(blue * 255);
+	return (color);
+}
+
+int		extract_colors(int color)
+{
+	double	red;
+	double	green;
+	double	blue;
+
+	red = (color >> 16) & 255;
+	green = (color >> 8) & 255;
+	blue = (color & 255);
 }
--- event_handlers.c
+++ event_handlers.c
@@ -6,26 +6,91 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:46:04 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/06/28 11:48:43 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/08/04 10:49:53 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	insidewin(int keycode, t_data *hook)
+int	esc(int keycode, t_data *hook)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(hook->mlx, hook->win);
+		exit(0);
+	}
+	(void)keycode;
+	return (0);
+}
+
+int key_hook(int keycode, t_data *hook)
+{
+	(void)keycode;
+	(void)hook;
+	return (0);
+}
+
+int	mouse_move(int button, int x, int y, t_data *hook)
+{
+	if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50))
+		printf("button clicked\n");
+	(void)hook;
+	(void)button;
+	return (0);
+}
+
+int xbutton(t_data *hook)
 {
 	mlx_destroy_window(hook->mlx, hook->win);
-	printf("keycode: %d\n", keycode);
-	(void)keycode;
 	exit(0);
 	return (0);
 }
 
-int	xbutton(int button, int x, int y, t_data *hook)
+int	mouse_hook(int keycode, int button, int x, int y, t_data *hook)
 {
-	if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50))// <= 1920) && (y >= 0 && y <= 1080))
-		printf("button clicked\n");
-	(void)hook;
+	(void)x;
+	(void)y;
 	(void)button;
+	(void)keycode;
+	mlx_destroy_window(hook->mlx, hook->win);
+	exit(0);
+	return (0);
+}
+
+int		controls(int keycode, t_data *hook)
+{
+	int	x;
+	int y;
+	x = 935;
+	y = 1035;
+	int z = 100;
+	// draw_square(hook, x, y, z, 'x');
+	// z = 200;
+	// draw_square(hook, x, y, z, 'x');
+	// y = 100;
+	// draw_square(hook, x, y, z, 'y');
+	// x = 1035;
+	// draw_square(hook, x, y, z, 'y');
+	printf("keycode: %d\n", keycode);
+	if (keycode == 13) // W (y goes up)
+	{
+		y++;
+		draw_square(hook, x, y, z);//, 'x');
+	}
+//	else if (keycode == 0) // A (x goes down)
+//		x++;
+//	else if (keycode == 1) // S (y goes down)
+//		y--;
+//	else if (keycode == 2) // D (x goes up)
+//		x++;
+//	x = 935;
+//	y = 1035;
+//	z = 200;
+//	draw_square(hook, x, y, z, 'x');
+//	y = 100;
+//	draw_square(hook, x, y, z, 'y');
+//	x = 1035;
+//	draw_square(hook, x, y, z, 'y');
+	mlx_put_image_to_window(hook->mlx, hook->win, hook->img, x, y);
 	return (0);
 }
