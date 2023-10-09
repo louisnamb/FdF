811254a3bd7c843044db1744d5637dba2f159b1d
louisnamb <63900979+louisnamb@users.noreply.github.com>
2023-10-09 16:01:56 +1030

    Add files via upload

--- includ/fdf.h
+++ includ/fdf.h
@@ -6,119 +6,120 @@
 /*   By: lnambaji <lnambaji@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 11:46:29 by lnambaji          #+#    #+#             */
-/*   Updated: 2023/08/29 17:14:54 by lnambaji         ###   ########.fr       */
+/*   Updated: 2023/10/09 15:54:30 by lnambaji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <math.h>
-# include "mlx.h"
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <time.h>
-# include <fcntl.h>
+#include <math.h>
+#include "mlx.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
 
-typedef struct	map_info {
+typedef struct map_info {
 	int		r_cnt;
 	int		c_cnt;
 	int		r_pos;
 	int		c_pos;
 	int		**arr;
-}	details;
+}	t_details;
 
-typedef	struct	points {
-	int x_c;
-	int y_c;
-	int	line_add;
-	int	r;
-	int	c;
-	int add;
-}	pts_info;
+typedef struct points {
+	int		x_c;
+	int		y_c;
+	int		line_add;
+	int		r;
+	int		c;
+	int		add;
+}	t_pts_info;
 
-typedef struct	window_info {
+typedef struct shorten
+{
+	double	z;
+	double	c_p;
+	double	r_m;
+	double	c_m;
+	double	b_m;
+}	t_shorten;
+
+typedef struct window_info {
 	void			*mlx;
 	void			*img;
 	void			*win;
 	void			*addr;
-	int				bitsperpix;
-	int				linelen;
-	int				endian;
+	int				bbp;
+	int				llen;
+	int				end;
 	int				width;
-	int				height;
+	int				h;
 	struct map_info	*map;
 	struct points	*pts;
 }	t_data;
 
-typedef	struct bresenham {
-	int deltaX;
-	int deltaY;
+typedef struct read {
+	int		fd;
+	char	**split;
+	char	*path;
+	char	*eachline;
+}	t_read_utils;
+
+typedef struct bresenham {
+	int	deltax;
+	int	deltay;
 	int	diff;
-	int x;
-	int y;
+	int	x;
+	int	y;
 	int	variable_i;
-} bresenham;
+}	t_bresenham;
 
-typedef	struct color {
-	double curr_z;
-	double prev_z;
-	int x;
-	int colour;
-}	gradient;
+typedef struct color {
+	double	curr_z;
+	double	next_z;
+	int		x;
+	int		vert;
+}	t_fade;
 
-typedef	struct rgb {
+typedef struct rgb {
 	unsigned char	r;
-	unsigned char	g;
 	unsigned char	b;
+	unsigned char	g;
 	unsigned char	a;
-} rgb;
+}	t_rgb;
 
-typedef	struct vector {
+typedef struct vector {
 	float	x;
 	float	y;
 	float	z;
-} vec;
+}	t_vec;
 
-typedef	struct	degrees {
+typedef struct degrees {
 	float	x;
 	float	y;
 	float	z;
-}	angles;
+}	t_angles;
 
-
-void		initialisation(t_data *mlx, char *filepath);
-
-int			keys_pressed(int keycode, t_data *hook);
-
-int			mouse_move(int button, int x, int y, t_data *hook);
-
-int			xbutton(t_data *hook);// int x, int y, t_data *hook);
-
-int			key_hook(int keycode, t_data *hook);
-
-int			mouse_hook(int keycode, int button, int x, int y, t_data *hook);
-
-int			controls(int keycode, t_data *hook);
-
-details		*read_map(char *filename);
-
-char		**ft_split(char const *s, char c);
-
-char		*get_next_line(int fd);
-
-char		*ft_strjoin(char const *s1, char const *s2);
-
-int			ft_atoi(const char *str);
-
-int			ft_isdigit(int c);
-
-void		mmlx_put_pix(t_data *data, int x, int y, int colour);
-
-void		draw_bresenham_line_l(t_data *mlx, vec *start, vec *end, gradient *color);
-
-void		draw_bresenham_line_h(t_data *mlx, vec *start, vec *end, gradient *color);
-
-void 		draw_line(void *mlx, void *win, int beginX, int beginY, int endX, int endY, int color);
-
-void		draw_vertical(t_data *mlx, vec *start, vec *end, int color);
-
+int			absolute(int result);
+void		colorise(t_data *data, int x, int y, int colour);
+void		draw_b_l(t_data *mlx, t_vec *start, t_vec *end, t_fade *color);
+void		draw_b_h(t_data *mlx, t_vec *start, t_vec *end, t_fade *color);
+void		draw_vertical(t_data *mlx, t_vec *start, t_vec *end, int color);
 void		draw_grid(t_data *mlx);
+int			end(t_data *hook);
+char		*ft_strjoin(char const *s1, char const *s2);
+int			ft_atoi(const char *str);
+int			ft_isdigit(int c);
+void		initialisation(t_data *mlx, char *filepath);
+int			key_hook(int keycode, t_data *hook);
+int			keys_pressed(int keycode, t_data *hook);
+char		**ft_split(char const *s, char c);
+char		*get_next_line(int fd);
+int			mouse_move(int button, int x, int y, t_data *hook);
+t_details	*read_map(char *filename);
+t_vec		r_xyz(t_vec *new, t_angles theta);
+t_vec		rx_xyz(t_vec mat, float theta);
+t_vec		ry_xyz(t_vec mat, float theta);
+t_vec		rz_xyz(t_vec mat, float theta);
+int			shader(t_fade *color, int start, int end, int curr);
+void		swap_points(t_data *mlx, t_vec *start, t_vec *end, t_fade *color);
